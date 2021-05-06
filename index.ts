90071a617a287b80087d3a65557f96721bc25f50
Lachlan Miller <lachlan.miller.1990@outlook.com>
2021-05-06 11:06:16 +1000

    ermove watch mode

--- index.ts
+++ index.ts
@@ -2,42 +2,18 @@
 
 import fs from "fs";
 import minimist from "minimist";
-import chokidar from "chokidar";
 import { run } from "./api";
 
 const argv = minimist(process.argv.slice(2));
 
-if (!argv._.length && !argv.watch) {
+if (!argv._.length) {
   console.error("No test file provided.");
   process.exit(1);
 }
 
-// watch all
-if (!argv._.length && argv.watch) {
-  const loc = process.cwd();
-  console.log(`Watching for changes in ${loc} ...`);
-  const watcher = chokidar.watch(loc, {
-    ignored: ["node_modules"],
-  });
-
-  watcher.on("change", (file) => {
-    console.clear();
-    const buf = fs.readFileSync(file, "utf-8");
-    eval(buf);
-    run();
-  });
-}
-
-// watch specific file(s)
-if (argv._.length && argv.watch) {
-  throw Error('Watching specific files is not supported yet.')
-}
-
 // run specific file(s)
-if (argv._.length && !argv.watch) {
-  for (const file of argv._) {
-    const buf = fs.readFileSync(file, "utf-8");
-    eval(buf);
-  }
-  run();
+for (const file of argv._) {
+  const buf = fs.readFileSync(file, "utf-8");
+  eval(buf);
 }
+run();
