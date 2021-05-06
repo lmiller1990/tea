f21b739bbbdcabd3926bb4ca27a7d19557566d9a
Lachlan Miller <lachlan.miller.1990@outlook.com>
2021-05-06 18:01:39 +1000

    wip

--- b.js
+++
@@ -1,7 +0,0 @@
-const { it, describe, expect } = require("../");
-
-describe("Suite b", () => {
-  it("b is not a", () => {
-    expect("b").toBe("a");
-  });
-});
--- basic.js
+++
@@ -1,8 +0,0 @@
-const { it, describe, expect } = require("./api.js");
-
-describe("Nested", () => {
-  it("foo should be foo", () => {
-    const blah = "foo"
-    expect("foooo").toBe(blah);
-  });
-});
